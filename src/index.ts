9a4689949aef6276192a13fa06b5a887c181a67a
Sergei Kudriavtsev <kudryavtsevsr@gmail.com>
2024-01-15 14:35:52 -0300

    fix rules-of-hooks and improve typification

--- src/index.ts
+++ src/index.ts
@@ -2,20 +2,22 @@ import {useEffect, useRef} from 'react';
 
 export const TEST_RERENDER_ATTRIBUTE_NAME = 'data-test-render';
 
+export type TestRerenderHook = { bind: {[TEST_RERENDER_ATTRIBUTE_NAME]: number} | {}}
+
+export function useTestRerender(): TestRerenderHook
+
 export function useTestRerender() {
+  const renderCount = useRef(0);
+
+  useEffect(() => {
+    renderCount.current++;
+  });
+
   if (!['development', 'test'].includes(process.env.NODE_ENV)) {
     return {
       bind: {}
     };
   }
-
-  // eslint-disable-next-line react-hooks/rules-of-hooks
-  const renderCount = useRef(0);
-
-  // eslint-disable-next-line react-hooks/rules-of-hooks
-  useEffect(() => {
-    renderCount.current++;
-  });
 
   return {
     bind: {
